0030f847076955dddaf4c70b8120d1433e699212
artem <artialbk@gmail.com>
2024-10-19 07:20:30 +0300

    done

---
+++ script.js
@@ -0,0 +1,40 @@
+// задание 4
+
+
+// function extractCurrencyValue(str) {
+//     return +str.slice(1);
+//   }
+// console.log(extractCurrencyValue("$200"))
+
+
+
+//  задание 1
+
+
+// function changeFirstSimbol(str){
+//     let newStr=str[0].toUpperCase()+str.slice(1)
+//     return newStr
+// }
+// alert(changeFirstSimbol("bye"));
+
+
+
+// задание 3
+
+
+// function truncate(str, maxlength) {
+//     return (str.length > maxlength) ?
+//       str.slice(0, maxlength - 1) + '…' : str;
+//   }
+
+
+
+// function checkSpam(str) {
+//     let lowerStr = str.toLowerCase();
+  
+//     return lowerStr.includes('viagra') || lowerStr.includes('xxx');
+//   }
+  
+//   alert( checkSpam('buy ViAgRA now') );
+//   alert( checkSpam('free xxxxx') );
+//   alert( checkSpam("innocent rabbit") );
